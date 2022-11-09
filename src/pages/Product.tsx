10a7e18fa83867dace8ef043c35b2bbffa8458f9
cloworm <chloe.echikson@gmail.com>
2022-11-08 21:26:58 -0500

    displaying total on product page

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,10 +1,25 @@
 import { FunctionComponent } from 'react'
 import { useParams } from 'react-router-dom'
+import { useQuery } from '@tanstack/react-query'
+import Typography from '@mui/material/Typography'
+
+import { Product as ProductType } from './Products'
 
 const Product: FunctionComponent = () => {
   const { productId } = useParams()
+  const { isLoading, error, data } = useQuery<ProductType, Error>({
+    queryKey: [`product${productId}`],
+    queryFn: () =>
+      fetch(`https://fakestoreapi.com/products/${productId}`).then(res =>
+        res.json()
+      )
+  })
 
-  return (<div>{productId}</div>)
+  if (isLoading) return (<Typography>Loading...</Typography>)
+
+  if (error) return (<Typography>An error has occurred: {error.message}</Typography>)
+
+  return (<div>Total for {productId}: {data.price}</div>)
 }
 
 export default Product
