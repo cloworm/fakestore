e04406bcaa6dbb64da1393533b57950e4fb5a0d4
cloworm <chloe.echikson@gmail.com>
2022-11-08 23:38:17 -0500

    added link to view products from home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import { FunctionComponent } from 'react'
 import Typography from '@mui/material/Typography'
+import { Link as RouterLink } from 'react-router-dom'
 
 const Home: FunctionComponent = () => {
   return (
     <div>
-      <Typography>Home</Typography>
+      <Typography gutterBottom>Home</Typography>
+      <Typography>Welcome to fakestore. View our <RouterLink to="/products">Products</RouterLink>.</Typography>
     </div>
   )
 }
