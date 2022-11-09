41b1ab6fc39b60c8c6760ebf4074022198e18700
cloworm <chloe.echikson@gmail.com>
2022-11-08 23:19:49 -0500

    created basic cards for products page

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -5,6 +5,13 @@ import Typography from '@mui/material/Typography'
 import Stack from '@mui/material/Stack'
 import Breadcrumbs from '@mui/material/Breadcrumbs'
 import Link from '@mui/material/Link'
+import Card from '@mui/material/Card'
+import CardMedia from '@mui/material/CardMedia'
+import CardContent from '@mui/material/CardContent'
+import CardActions from '@mui/material/CardActions'
+import Chip from '@mui/material/Chip'
+import Box from '@mui/material/Box'
+import { styled } from '@mui/material'
 
 export interface Rating {
   rate: number;
@@ -20,6 +27,41 @@ export interface Product {
   image: string;
   rating: Rating;
 }
+
+const ProductsContainer = styled(Box)`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  margin-top: 20px;
+  column-gap: 20px;
+  row-gap: 50px;
+` as typeof Box
+
+const CardTitle = styled(Link)`
+  color: #1976d2;
+  font-weight: 900;
+  line-height: 1.4rem;
+  margin-bottom: 10px;
+  display: block;
+` as typeof Link
+
+const CardDescription = styled(Typography)`
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;  
+  overflow: hidden;
+` as typeof Typography
+
+const CardBox = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+` as typeof Box
+
+const ProductCard = styled(Card)`
+  display: flex;
+  flex-direction: column;
+` as typeof Card
 
 const Products: FunctionComponent = () => {
   const { isLoading, error, data } = useQuery<Product[], Error>({
@@ -53,17 +95,37 @@ const Products: FunctionComponent = () => {
           {breadcrumbs}
         </Breadcrumbs>
       </Stack>
-      <ul>
+      <ProductsContainer>
         {
           data.map((row) => {
             return (
-              <li key={row.id}>
-                <RouterLink to={`/products/${row.id}`}>{row.title}</RouterLink>
-              </li>
+              <ProductCard sx={{ maxWidth: 345 }} key={row.id}>
+                <RouterLink to={`/products/${row.id}`}>
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    image={row.image}
+                    alt={row.title}
+                  />
+                </RouterLink>
+                <CardBox>
+                  <CardContent>
+                    <CardTitle component={RouterLink} to={`/products/${row.id}`} key={row.id} underline="hover" variant="h6">
+                      {row.title}
+                    </CardTitle>
+                    <CardDescription variant="body2" color="text.secondary">
+                      {row.description}
+                    </CardDescription>
+                  </CardContent>
+                  <CardActions>
+                    <Chip label={row.category} color="primary" variant="outlined" />
+                  </CardActions>
+                </CardBox>
+              </ProductCard>
             )
           })
         }
-      </ul>
+      </ProductsContainer>
     </>
   )
 }
